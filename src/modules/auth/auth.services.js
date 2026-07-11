622888834e2df4633bbdeea3c4ec7023307d75ee
sandeepr <sandeeprohillainfo@gmail.com>
2026-07-11 08:46:19 +0530

    changes: - Added boilerplate for token - Added token - Write the first services

--- src/modules/auth/auth.services.js
+++ src/modules/auth/auth.services.js
@@ -1,10 +1,28 @@
 import ApiError from "../../common/utils/api-error.js"
+import { generateResetToken } from "../../common/utils/jwt.utils.js";
 import User from "./auth.models.js"
 
 const register = async ({ name, email, password }) =>{
     const existingUser = User.findOne({ email });
 
-    if(existingUser) throw ApiError.
+    if(existingUser) throw ApiError.conflict("Email already exisits")
+
+    const { rawToken, hashedToken } = generateResetToken()
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        verificationToken: hashedToken
+    })
+
+    const userObj = user.toObject()
+    delete userObj.password
+    delete userObj.verificationToken
+
+    // send an email to user with rawToken
+
 }
 
 export { register }
